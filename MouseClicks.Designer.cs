e2571a0c5c0c5e279e441129fd690c6ee267c1aa
Ignacio Nieto <nietofarias@gmail.com>
2014-04-07 01:01:33 -0300

    first commit

---
+++ MouseClicks.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MouseClicks
+{
+    partial class MouseClicksForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.YPos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.XPos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ThrowDice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // YPos
+            // 
+            this.YPos.Location = new System.Drawing.Point(121, 25);
+            this.YPos.Name = "YPos";
+            this.YPos.Size = new System.Drawing.Size(39, 20);
+            this.YPos.TabIndex = 9;
+            this.YPos.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X:";
+            // 
+            // XPos
+            // 
+            this.XPos.Location = new System.Drawing.Point(121, -1);
+            this.XPos.Name = "XPos";
+            this.XPos.Size = new System.Drawing.Size(39, 20);
+            this.XPos.TabIndex = 6;
+            this.XPos.Text = "680";
+            // 
+            // MouseClicksForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(162, 47);
+            this.Controls.Add(this.YPos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.XPos);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MouseClicksForm";
+            this.Text = "MouseControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox YPos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox XPos;
+    }
+}
+
---
+++ MouseClicks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MouseClicks
+{
+    public partial class MouseClicksForm : Form
+    {
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public MouseClicksForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] destino = {Int32.Parse(this.XPos.Text), Int32.Parse(this.YPos.Text)};
+            int[] original = this.obtenerPosicionMouse();
+            this.clickear(destino);
+            this.mover(original);
+        }
+
+        private int[] obtenerPosicionMouse() {
+            return new int[] { Cursor.Position.X, Cursor.Position.Y};
+        }
+
+        private void clickear(int[] posicion) {
+            Cursor.Position = new Point(posicion[0], posicion[1]);
+            var input = new INPUT() { dwType = 0, mi = new MOUSEINPUT() { dwFlags = MOUSEEVENTF_LEFTDOWN} };
+            SendInput(1, new INPUT[]{input}, Marshal.SizeOf(input));
+            input = new INPUT() { dwType = 0, mi = new MOUSEINPUT() { dwFlags = MOUSEEVENTF_LEFTUP } };
+            SendInput(1, new INPUT[]{input}, Marshal.SizeOf(input));
+        }
+
+        private void mover (int[] posicion) {
+            Cursor.Position = new Point(posicion[0], posicion[1]);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT {int dx; int dy; int mouseData; public int dwFlags; int time; IntPtr dwExtraInfo; }
+        struct INPUT { public uint dwType; public MOUSEINPUT mi; }
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    }
+}
